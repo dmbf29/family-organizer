d909805fcc326610f3134b2c32b47d4197a7690d
Douglas Berkley <douglasmberkley@gmail.com>
2019-04-04 17:05:38 +0900

    Added child model

---
+++ db/migrate/20190404080532_create_children.rb
@@ -0,0 +1,13 @@
+class CreateChildren < ActiveRecord::Migration[5.2]
+  def change
+    create_table :children do |t|
+      t.references :marriage, foreign_key: true
+      t.string :first_name
+      t.date :birthday
+      t.string :photo
+      t.string :relation
+
+      t.timestamps
+    end
+  end
+end
