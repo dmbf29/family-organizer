5cbf569756c262c3df1c49590fd8d3f4c377857a
Douglas Berkley <douglasmberkley@gmail.com>
2019-04-05 14:34:42 +0900

    seeds bug fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Child.destroy_all
+Marriage.destroy_all
+Male.destroy_all
+Female.destroy_all
+
 chris = Male.new(
   first_name: "Chris",
   last_name: "Lorbert",
@@ -20,3 +25,13 @@ chris_ginny = Marriage.new(
   anniversay: Date.new(2014, 05, 22),
   remote_photo_url: "https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/54257146_10107103659775820_5541410184468889600_o.jpg?_nc_cat=108&_nc_ht=scontent-nrt1-1.xx&oh=c676ec3386b93cabbc5def7393a02033&oe=5D3BD081"
 )
+chris_ginny.save!
+
+cian = Child.new(
+  first_name: "Cian",
+  middle_name: "Grady",
+  birthday: Date.new(2019, 03, 15),
+  marriage: chris_ginny,
+  remote_photo_url: "https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/53835422_10107103408978420_4360084781233340416_o.jpg?_nc_cat=104&_nc_ht=scontent-nrt1-1.xx&oh=2e9b18b0ce668e0ff4d8ccf67cf5da54&oe=5D425287"
+)
+cian.save!
