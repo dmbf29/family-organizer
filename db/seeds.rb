32a189e2bcf525a1c3617c4966dbc2efe027c0e5
Douglas Berkley <douglasmberkley@gmail.com>
2019-04-05 14:01:08 +0900

    Added cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Male.create!(
+#   first_name: "Chris",
+#   last_name: "Lorbert",
+#   birthday: Date.new(1986, 02, 13),
+#   remote_photo_url:
+# )
